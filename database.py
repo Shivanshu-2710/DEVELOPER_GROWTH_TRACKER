46a6efff0f9384c1348cea931132e383ceaccf83
Shivanshu-2710 <sbshivanshu2000@gmail.com>
2026-07-08 15:48:57 +0530

    database creator made

--- database.py
+++ database.py
@@ -4,7 +4,25 @@ db=ms.connect(host='localhost',
               password='Shivanshu@mysql',
               database='growth_tracker')
 cursor=db.cursor()
-cursor.execute('insert into users(name) VALUES(%s)',('Shivanshu',))
-db.commit()
-print('user added succesfully')
-db.close()  
+cursor.execute('''
+CREATE TABLE daily_progress2 (
+    id INT AUTO_INCREMENT,
+    user_id INT,
+    study_hours FLOAT,
+    dsa_questions INT,
+    git_commits INT,
+    project_hours FLOAT,
+    progress_date DATE,
+    PRIMARY KEY (id)
+);
+''')
+cursor.execute('''
+CREATE TABLE users2(
+    id INT AUTO_INCREMENT,
+    user_id INT,
+    user_name varchar(100),
+    PRIMARY KEY (id)
+);  
+''')
+print('done')
+db.close()

49091baf660bcbd4a625da9de13bd77926275ab6
Shivanshu-2710 <sbshivanshu2000@gmail.com>
2026-07-08 16:10:57 +0530

    completed csvexport feature

--- csvexport.py
+++ csvexport.py
@@ -6,9 +6,18 @@ db=ms.connect(host='localhost',
               database='growth_tracker')
 cursor=db.cursor()
 u_id=int(input("Enter User Id"))
-cursor.execute('select * from daily_progress where user_id like %s',(u_id,))
-records=cursor.fetchall()
-with open('growthtracker.csv','w') as f:
-    writer=csv.writer(f)
-    writer.writerow(['USER_ID','Study_Hours','Dsa_Questions','Git_Commits','Projecta_Hours','Progress_Date'])
-    writer.writerows(records)
+cursor.execute('''select user_id,
+    study_hours,
+    dsa_questions,
+    git_commits,
+    project_hours,
+    progress_date from daily_progress where user_id like %s''',(u_id,))
+with open('growthtracker.csv','w',newline='') as f:
+        writer=csv.writer(f)
+        writer.writerow(['USER_ID','Study_Hours','Dsa_Questions','Git_Commits','Projecta_Hours','Progress_Date'])
+record=cursor.fetchone()
+while record is not None:
+    with open('growthtracker.csv','a',newline='') as f:
+        writer=csv.writer(f)
+        writer.writerow(record)
+        record=cursor.fetchone()

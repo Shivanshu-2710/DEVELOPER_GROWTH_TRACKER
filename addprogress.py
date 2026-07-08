57d3c03b612c828c04d36178b3d5d0c86f516fec
Shivanshu-2710 <sbshivanshu2000@gmail.com>
2026-07-08 15:55:36 +0530

    little changes in addprogress

--- addprogress.py
+++ addprogress.py
@@ -9,7 +9,7 @@ sh=float(input("Study hours : "))
 dq=int(input("Enter dsa questions : "))
 gc=int(input("Git Commits : "))
 ph=float(input("Enter project hours :"))
-pd=input("Enter Date (YYYY:MM:DD)")
+pd=input("Enter Date (YYYY-MM-DD)")
 cursor.execute("insert into daily_progress(user_id,study_hours,dsa_questions,git_commits,project_hours,progress_date) VALUES(%s,%s,%s,%s,%s,%s)",
                (user_id,sh,dq,gc,ph,pd))
 db.commit()

6200bd30aeb2f086b80e710cd3465fd9e7871d2e
Shivanshu-2710 <sbshivanshu2000@gmail.com>
2026-07-14 13:06:17 +0530

    exception handling modified in add progress feature

--- addprogress.py
+++ addprogress.py
@@ -1,23 +1,39 @@
 import mysql.connector as ms
-db=ms.connect(host="localhost",
+from datetime import datetime
+def validate_non_negative(a):
+    if a<0:
+        raise ValueError("NEGATIVE VALUES ARE NOT ALLOWED")
+    else:
+        return a
+while True:
+    try:
+        db=ms.connect(host="localhost",
               user='root',
               password='Shivanshu@mysql',
               database='growth_tracker')
-cursor=db.cursor()
-while True:
-    try:
+        cursor=db.cursor()
         user_id=int(input("Enter your userid : "))
+        user_id=validate_non_negative(user_id)
         sh=float(input("Study hours : "))
+        sh=validate_non_negative(sh)
         dq=int(input("Enter dsa questions : "))
+        dq=validate_non_negative(dq)
         gc=int(input("Git Commits : "))
+        gc=validate_non_negative(gc)
         ph=float(input("Enter project hours : "))
+        ph=validate_non_negative(ph)
         pd=input("Enter Date (YYYY-MM-DD) : ")
+        pd = datetime.strptime(pd, "%Y-%m-%d")
+        print(pd)
         cursor.execute("insert into daily_progress(user_id,study_hours,dsa_questions,git_commits,project_hours,progress_date) VALUES(%s,%s,%s,%s,%s,%s)",
                 (user_id,sh,dq,gc,ph,pd))
         db.commit()
         print('DONE')
-        db.close()
         break
-    except:
-        print('Please Enter Valid Values !!')
-        continue
+    except ValueError as e:
+        print(e)
+    except ms.Error as e:
+        print('DATABASE ERROR ',e)
+        db.rollback()
+    finally:
+        db.close()
